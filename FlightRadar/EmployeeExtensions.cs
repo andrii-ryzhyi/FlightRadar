1a884887836998bbbbdc33a59521618d7123f0af
Andrii.Ryzhyi <andrii.nrj@gmail.com>
2020-04-06 21:14:16 +0300

    Add Extension method and additional delegate

---
+++ FlightRadar/EmployeeExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightRadar
+{
+    public static class EmployeeExtensions
+    {
+        public static void OnInternalNotification(this Employee employee, Notification notification)
+        {
+            Console.WriteLine("Internal Employee notification: {0}", notification.Message);
+        }
+    }
+}
